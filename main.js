e7747af22a756a200702caffe0e2a40b3a730974
jestapinski <jstapins@andrew.cmu.edu>
2015-10-06 19:27:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 "Hello World"
 "Does the committing work?"
 "Hey all, if you can see this than GitHub is working correctly"
-<<<<<<< HEAD
 "Check 4"
-=======
->>>>>>> 7ed0fbfb2b4e161e1cc6750fe9b6044d56890319
+"Check 5"
